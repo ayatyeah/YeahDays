206bc3df8b9e398cdec074956982be54c3841ca3
YeahAyat <balmagambetaat@gmail.com>
2026-03-06 23:04:53 +0500

    v1.8.2 fix api/health

--- vite.config.ts
+++ vite.config.ts
@@ -36,6 +36,7 @@ export default defineConfig({
       },
       workbox: {
         globPatterns: ['**/*.{js,css,html,svg,png,ico}'],
+        navigateFallbackDenylist: [/^\/api\//, /^\/auth\//, /^\/data\//],
       },
     }),
   ],
